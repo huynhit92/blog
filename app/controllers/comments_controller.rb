4bf65a5b1166f9e1aab4004dfd645676147cc7c1
Pham Tran Huynh <t-huynh@oa.netmile.lan>
2016-04-05 00:37:34 +0900

    add somthing

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_param)
+
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+  private
+    def comment_param
+      params.require(:comment).permit(:name, :body)
+    end
+end
